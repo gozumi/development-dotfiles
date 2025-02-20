983fe6a1a35d77e049844c1bc3a5fd93f98f4882
Ian Anderson <ian.kirk.anderson@gmail.com>
2025-02-20 22:56:43 +0000

    Streamline the repo as a dotfiles repo.

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+echo "Installing dotfiles."
+
 sudo apt-get update
 sudo apt-get install ansible -y
 
-ansible-playbook ~/development-vm/bootstrap.yml
+ansible-playbook ./bootstrap.yml
